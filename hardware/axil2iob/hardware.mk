fd72bcbd1f9fda2cbed8d31a72540a0f74c2642e
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2022-02-07 23:11:29 +0000

    add missing include

--- hardware/axil2iob/hardware.mk
+++ hardware/axil2iob/hardware.mk
@@ -3,6 +3,8 @@ ifneq (axil2iob,$(filter axil2iob, $(HW_MODULES)))
 # Add to modules list
 HW_MODULES+=axil2iob
 
+INCLUDE+=$(AXI_DIR)/hardware/include
+
 # Sources
 VSRC+=$(AXI_DIR)/hardware/axil2iob/axil2iob.v
 
