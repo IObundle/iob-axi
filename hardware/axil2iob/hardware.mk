3d2b8a5b44c5f40092bea110801ee09e87efd7a0
Joao D Lopes <joao.d.lopes91@gmail.com>
2022-02-07 17:45:08 +0000

    update source paths; move Makefile to root directory; add AXI_MEM submodule

--- hardware/axil2iob/hardware.mk
+++ hardware/axil2iob/hardware.mk
@@ -4,6 +4,6 @@ ifneq (axil2iob,$(filter axil2iob, $(HW_MODULES)))
 HW_MODULES+=axil2iob
 
 # Sources
-VSRC+=$(LIB_DIR)/hardware/axil2iob/axil2iob.v
+VSRC+=$(AXI_DIR)/hardware/axil2iob/axil2iob.v
 
 endif
--- hardware/iob2axi/hardware.mk
+++ hardware/iob2axi/hardware.mk
@@ -3,9 +3,12 @@ ifneq (iob2axi,$(filter iob2axi, $(HW_MODULES)))
 # Add to modules list
 HW_MODULES+=iob2axi
 
+# Includes
+INCLUDE+=$(incdir)$(AXI_DIR)/hardware/include
+
 # Sources
-VSRC+=$(LIB_DIR)/hardware/iob2axi/iob2axi.v \
-$(LIB_DIR)/hardware/iob2axi/iob2axi_wr.v \
-$(LIB_DIR)/hardware/iob2axi/iob2axi_rd.v
+VSRC+=$(AXI_DIR)/hardware/iob2axi/iob2axi.v \
+$(AXI_DIR)/hardware/iob2axi/iob2axi_wr.v \
+$(AXI_DIR)/hardware/iob2axi/iob2axi_rd.v
 
 endif
